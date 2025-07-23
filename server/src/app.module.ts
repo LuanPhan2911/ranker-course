f00e536e8f5c8be57e7467b6b7ca672e2bce0427
Luan@YokKok <97419663+LuanPhan2911@users.noreply.github.com>
2025-07-23 18:50:08 +0700

    redis connect

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PollModule } from './polls/poll.module';
+import { RegisterRedisModule } from './redis.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), PollModule],
+  imports: [ConfigModule.forRoot(), PollModule, RegisterRedisModule],
   controllers: [],
   providers: [],
 })
---
+++ server/src/redis.module.ts
@@ -0,0 +1,78 @@
+import { DynamicModule, FactoryProvider, ModuleMetadata } from '@nestjs/common';
+import { Module } from '@nestjs/common';
+import IORedis, { Redis, RedisOptions } from 'ioredis';
+
+export const IORedisKey = 'IORedis';
+
+type RedisModuleOptions = {
+  connectionOptions: RedisOptions;
+  onClientReady?: (client: Redis) => void;
+};
+
+type RedisAsyncModuleOptions = {
+  useFactory: (
+    ...args: any[]
+  ) => Promise<RedisModuleOptions> | RedisModuleOptions;
+} & Pick<ModuleMetadata, 'imports'> &
+  Pick<FactoryProvider, 'inject'>;
+
+@Module({})
+class RedisModule {
+  static async registerAsync({
+    useFactory,
+    imports,
+    inject,
+  }: RedisAsyncModuleOptions): Promise<DynamicModule> {
+    const redisProvider = {
+      provide: IORedisKey,
+      useFactory: async (...args) => {
+        const { connectionOptions, onClientReady } = await useFactory(...args);
+
+        const client = await new IORedis(connectionOptions);
+
+        onClientReady(client);
+
+        return client;
+      },
+      inject,
+    };
+
+    return {
+      module: RedisModule,
+      imports,
+      providers: [redisProvider],
+      exports: [redisProvider],
+    };
+  }
+}
+
+import { Logger } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+export const RegisterRedisModule = RedisModule.registerAsync({
+  imports: [ConfigModule],
+  useFactory: async (configService: ConfigService) => {
+    const logger = new Logger('RedisModule');
+
+    return {
+      connectionOptions: {
+        host: configService.get('REDIS_HOST'),
+        port: configService.get('REDIS_PORT'),
+      },
+      onClientReady: (client) => {
+        logger.log('Redis client ready');
+
+        client.on('error', (err) => {
+          logger.error('Redis Client Error: ', err);
+        });
+
+        client.on('connect', () => {
+          logger.log(
+            `Connected to redis on ${client.options.host}:${client.options.port}`,
+          );
+        });
+      },
+    };
+  },
+  inject: [ConfigService],
+});
