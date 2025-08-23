2329760b5b9a6eb9ea9f9595990c377bd35611c2
Luan@YokKok <97419663+LuanPhan2911@users.noreply.github.com>
2025-08-23 14:31:08 +0700

    socket connection

--- server/src/polls/poll-gateway.ts
+++ server/src/polls/poll-gateway.ts
@@ -1,16 +1,40 @@
 import { Logger } from '@nestjs/common';
-import { OnGatewayInit, WebSocketGateway } from '@nestjs/websockets';
+import {
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
 import { PollService } from './poll.service';
+import { Namespace, Socket } from 'socket.io';
 
 @WebSocketGateway({
   namespace: 'polls',
 })
-export class PollGateway implements OnGatewayInit {
+export class PollGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
   private readonly logger = new Logger(PollGateway.name);
+  @WebSocketServer() io: Namespace;
   constructor(private readonly pollService: PollService) {}
 
   // Gateway initialized (provided in module and instantiated)
   afterInit(): void {
     this.logger.log(`Websocket Gateway initialized.`);
+  }
+  handleConnection(client: Socket, ...args: any[]) {
+    const sockets = this.io.sockets;
+
+    this.logger.log(`WS Client with id: ${client.id} connected!`);
+    this.logger.debug(`Number of connected sockets: ${sockets.size}`);
+
+    this.io.emit('hello', `hello socket ${client.id}`);
+  }
+  handleDisconnect(client: Socket) {
+    const sockets = this.io.sockets;
+
+    this.logger.log(`Disconnected socket id: ${client.id}`);
+    this.logger.debug(`Number of connected sockets: ${sockets.size}`);
   }
 }
