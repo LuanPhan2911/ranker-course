72afd66e09eb5be379113ab8e9c77b6827ffe435
Luan@YokKok <97419663+LuanPhan2911@users.noreply.github.com>
2025-08-29 10:11:04 +0700

    poll form

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,8 +4,18 @@ import './index.css';
 
 import Pages from './pages';
 import { state } from './state';
+import Loader from './components/ui/Loader';
+import { useSnapshot } from 'valtio';
 
 devtools(state, 'app state');
-const App: React.FC = () => <Pages />;
+const App: React.FC = () => {
+  const currentState = useSnapshot(state);
+  return (
+    <>
+      <Loader isLoading={currentState.isLoading} color="orange" width={120} />
+      <Pages />
+    </>
+  );
+};
 
 export default App;
